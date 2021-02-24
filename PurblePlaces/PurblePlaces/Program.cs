8fd249fb9dce1b3d23da4433aa814aa971520bb0
Shahrukh2123 <sh0450230@gmail.com>
2021-02-24 16:31:34 +0530

    Last Changes

---
+++ PurblePlaces/PurblePlaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PurblePlaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Shahrukh Khan!");
+        }
+    }
+}
